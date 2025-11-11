c77cbfd013e10768c041b78b71a9d7bef5431b8c
Azzam S.A <vcs@azzamsa.com>
2025-11-11 10:02:59 +0700

    feat(post_install): more packages

--- _docs/post_install/cargo.sh
+++ _docs/post_install/cargo.sh
@@ -13,7 +13,8 @@ cargo install cargo-binstall
 
 set -l pkgs \
     atuin dotter fnm starship just jj-cli gitu \
-    cargo-update cargo-edit cargo-outdated cargo-cache cargo-tarpaulin bacon dprint git-cliff hurl selene \
+    cargo-update cargo-udeps cargo-audit cargo-edit cargo-outdated cargo-cache cargo-tarpaulin \
+    bacon dprint git-cliff hurl selene \
     stylua typos-cli watchexec-cli git-cliff dprint typos-cli jaq bandwhich bat dua-cli kondo \
     yazi-fm eza typstyle typst-cli lolcrab macchina tokei motus \
     # Text Editor

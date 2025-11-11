c8f2e2c7b6df4226acc88268d0b4c454cce50f7c
Azzam S.A <vcs@azzamsa.com>
2025-11-11 10:02:59 +0700

    feat(post_install): add more packages

--- _docs/post_install/flatpak.sh
+++ _docs/post_install/flatpak.sh
@@ -1,23 +1,64 @@
 #!/usr/bin/env fish
-
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-#
-# Setup Core Packages
-set -l pkgs \
-    app.zen_browser.zen ca.desrt.dconf-editor com.google.Chrome \
-    com.calibre_ebook.calibre com.dec05eba.gpu_screen_recorder com.github.IsmaelMartinez.teams_for_linux \
-    com.github.finefindus.eyedropper com.github.johnfactotum.Foliate com.github.qarmin.czkawka \
-    com.github.tchx84.Flatseal com.mattjakeman.ExtensionManager com.usebruno.Bruno \
-    dev.vencord.Vesktop io.dbeaver.DBeaverCommunity io.github.flattool.Warehouse \
-    io.github.pieterdd.StretchBreak io.gitlab.news_flash.NewsFlash it.mijorus.gearlever \
-    net.nokyan.Resources org.atheme.audacious org.flameshot.Flameshot org.gimp.GIMP \
-    com.audacityteam.Audacity org.gnome.Evolution org.gnome.Firmware org.gnome.World.PikaBackup \
-    org.inkscape.Inkscape org.keepassxc.KeePassXC org.kde.okular org.sqlitebrowser.sqlitebrowser \
-    org.mozilla.Thunderbird org.telegram.desktop us.zoom.Zoom
+# Tier 1: Core Daily Tools
+set -l core_pkgs (
+# editors
+dev.zed.Zed
+
+    com.google.Chrome
+    org.telegram.desktop
+    com.github.IsmaelMartinez.teams_for_linux
+    us.zoom.Zoom
+    dev.vencord.Vesktop
+    org.keepassxc.KeePassXC
+    io.dbeaver.DBeaverCommunity
+    org.flameshot.Flameshot
+    com.github.tchx84.Flatseal
+    io.github.flattool.Warehouse
+)
+
+# Tier 2: Frequent Task-Specific Tools
+set -l task_pkgs (
+    org.mozilla.Thunderbird
+    org.gnome.Evolution
+    io.gitlab.news_flash.NewsFlash
+    net.nokyan.Resources
+    com.github.finefindus.eyedropper
+    org.kde.okular
+    org.sqlitebrowser.sqlitebrowser
+    org.gnome.meld
+    app.zen_browser.zen
+)
+
+# Tier 3: Project and Creative Tools
+set -l project_pkgs (
+    org.gimp.GIMP
+    org.inkscape.Inkscape
+    com.audacityteam.Audacity
+    com.dec05eba.gpu_screen_recorder
+    com.calibre_ebook.calibre
+    com.github.johnfactotum.Foliate
+    com.github.qarmin.czkawka
+    ca.desrt.dconf-editor
+    com.mattjakeman.ExtensionManager
+    it.mijorus.gearlever
+)
+
+# Tier 4: System Maintenance and Utilities
+set -l utility_pkgs (
+    org.gnome.Firmware
+    org.gnome.World.PikaBackup
+    io.github.pieterdd.StretchBreak
+    io.github.nokse22.Exhibit
+    org.atheme.audacious
+)
+
+# Combine all packages into a single list for installation
+set -l pkgs $core_pkgs $task_pkgs $project_pkgs $utility_pkgs
 
 for pkg in $pkgs
-    flatpak install flathub --assumeyes $pkg
+    flatpak install flathub --assumeyes "$pkg"
 end
 
 # Optionals

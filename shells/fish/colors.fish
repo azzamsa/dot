83ed8e9614757e4364504328d0a2032f5724ffe5
Azzam S.A <noreply@azzamsa.com>
2025-12-31 13:52:07 +0700

    style: nothing beats the clarity of modus themes

--- shells/fish/colors.fish
+++ shells/fish/colors.fish
@@ -1,4 +1,12 @@
-if [ $theme = catppuccin_mocha ]
+if [ $theme = modus_vivendi ]
+    set -g red ff5f59 # 7.48:1, #ff5f59
+    set -g green 44bc44 # 13.41:1, #44bc44
+    set -g yellow fec43f # 13.18:1 #fec43f
+    set -g orange dfaf7a # 10.55:1, #dfaf7a
+    set -g purple b6a0ff # 9.46:1, #b6a0ff
+    set -g cyan 00d3d0 # 13.42:1, #00d3d0
+    set -g pink feacd0 # 12.02:1, #feacd0
+else if [ $theme = catppuccin_mocha ]
     # Source:
     # - https://catppuccin.com/palette
 

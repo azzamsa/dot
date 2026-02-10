eff191d29e084145be1cf1e90eeae0ba3c5dd0ea
Azzam S.A <noreply@azzamsa.com>
2026-02-10 07:08:33 +0700

    style: add original modus theme

--- shells/fish/colors.fish
+++ shells/fish/colors.fish
@@ -1,29 +1,48 @@
 #!/usr/bin/env fish
 
 if [ $theme = modus_vivendi ]
-    set -g red     "ff5f59"  # 7.48:1,  "#ff5f59"
-    set -g green   "44bc44"  # 13.41:1, "#44bc44"
-    set -g yellow  "fec43f"  # 13.18:1, "#fec43f"
-    set -g orange  "dfaf7a"  # 10.55:1, "#dfaf7a"
-    set -g purple  "b6a0ff"  # 9.46:1,  "#b6a0ff"
-    set -g cyan    "00d3d0"  # 13.42:1, "#00d3d0"
-    set -g pink    "feacd0"  # 12.02:1, "#feacd0"
+    set -g red     "ff5f59"  # 7.48:1   #ff5f59
+    set -g green   "44bc44"  # 13.41:1  #44bc44
+    set -g yellow  "d0bc00"  # 10.87:1  #d0bc00
+    set -g orange  "dfaf7a"  # 10.55:1  #dfaf7a
+    set -g purple  "b6a0ff"  # 9.46:1   #b6a0ff
+    set -g cyan    "00d3d0"  # 13.42:1  #00d3d0
+    set -g pink    "feacd0"  # 12.02:1  #feacd0
+    set -g comment "989898"  # 7.27:1   #989898
 end
 
+if [ $theme = modus_vivendi_tinted ]
+    set -g red     "ff5f59"  # 7.48:1   #ff5f59
+    set -g green   "44bc44"  # 13.41:1  #44bc44
+    set -g yellow  "d0bc00"  # 10.87:1  #d0bc00
+    # set -g yellow  "fec43f"  # 13.18:1  #fec43f
+    set -g orange  "dfaf7a"  # 10.55:1  #dfaf7a
+    set -g purple  "b6a0ff"  # 9.46:1   #b6a0ff
+    set -g cyan    "00d3d0"  # 13.42:1  #00d3d0
+    set -g pink    "feacd0"  # 12.02:1  #feacd0
+    set -g comment "ef8386"  # 8.26:1   #ef8386
+end
+
+if [ $theme = ef_dark ]
+    set -g red     "ef6560"  # !6.71:1  #ef6560
+    set -g green   "0faa26"  # !6.79:1  #0faa26
+    set -g yellow  "bf9032"  # 7.25:1   #bf9032
+    set -g orange  "df8a5a"  # 7.92:1   #df8a5a
+    set -g purple  "af85ff"  # 7.62:1   #af85ff
+    set -g cyan    "4fbaef"  # 9.6:1    #4fbaef
+    set -g pink    "d369af"  # !6.4:1   #d369af
+    set -g comment "cf9f8f"  # 9.1:1    #cf9f8f
+end
+
+# https://github.com/folke/tokyonight.nvim/blob/main/extras/fish/tokyonight_moon.fish
 if [ $theme = tokyonight_moon ]
-    # Source:
-    # - https://github.com/folke/tokyonight.nvim/blob/main/extras/fish/tokyonight_moon.fish
-
-    set -g red     "ff757f"  # 8.1:1,   "#ff757f"
-    set -g green   "c3e88d"  # 15.24:1, "#c3e88d"
-    set -g yellow  "ffc777"  # 13.68:1, "#ffc777"
-    set -g orange  "ff966c"  # 9.83:1,  "#ff966c"
-    set -g purple  "fca7ea"  # 11.85:1, "#fca7ea"
-    set -g cyan    "86e1fc"  # 14.18:1, "#86e1fc"
-    set -g pink    "c099ff"  # 9.24:1,  "#c099ff"
+    set -g red     "ff757f"  # 8.1:1    #ff757f
+    set -g green   "c3e88d"  # 15.24:1  #c3e88d
+    set -g yellow  "ffc777"  # 13.68:1  #ffc777
+    set -g orange  "ff966c"  # 9.83:1   #ff966c
+    set -g purple  "fca7ea"  # 11.85:1  #fca7ea
+    set -g cyan    "86e1fc"  # 14.18:1  #86e1fc
+    set -g pink    "c099ff"  # 9.24:1   #c099ff
+    set -g comment "828BB8"  # 6.33:1   #828BB8 (fg-dark)
+    # set -g comment "636da6"  # 4.26:1   #636da6
 end
-
-# All the colors below are kept across theme.
-set -g selection 4a4f69 # #4a4f69 (modus-vivendi bg-active)
-set -g comment 989898 # 7.27:1, #989898 (modus-vivendi fg-dim)
-set -g foreground ffffff # 21:1, #ffffff

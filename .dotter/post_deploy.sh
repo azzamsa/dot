78a1a6b2d69d3928eaf6e2c9a786bad14c25f43d
Azzam S.A <noreply@azzamsa.com>
2026-04-01 02:48:01 +0700

    feat: `rfont` research is done
    finding best random font

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -4,5 +4,4 @@ CONFIG_FILE="$HOME/.config/emacs/config.el"
 FONT="{{font}}"
 
 sed -i "s|(setq +font-family \"[^\"]*\"|(setq +font-family \"$FONT\"|" "$CONFIG_FILE"
-
 echo "Done. set Emacs font to \"$FONT\""

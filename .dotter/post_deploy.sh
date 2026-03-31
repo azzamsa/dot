174bfee7c6432194eaf1a61eb7778a5219279521
Azzam S.A <noreply@azzamsa.com>
2026-03-31 11:04:48 +0700

    feat: auto-configure Emacs font

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+CONFIG_FILE="$HOME/.config/emacs/config.el"
+FONT="{{font}}"
+
+sed -i "s|(setq +font-family \"[^\"]*\"|(setq +font-family \"$FONT\"|" "$CONFIG_FILE"
+
+echo "Done. set Emacs font to \"$FONT\""

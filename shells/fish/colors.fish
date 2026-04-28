6ae8bee14e05dfc53513574c9a12cad2f41c3cf5
Azzam S.A <noreply@azzamsa.com>
2026-04-28 12:38:45 +0700

    style: add `catppuccin mocha`

--- shells/fish/colors.fish
+++ shells/fish/colors.fish
@@ -22,3 +22,16 @@ if [ $theme = tokyo_black ]
     set -g pink    "c099ff"  # 9.24:1   #c099ff
     set -g comment "9aa5ce"  # 8.65:1   #9aa5ce
 end
+
+
+# https://catppuccin.com/palette
+if [ $theme = catppuccin_mocha ]
+    set -g red     "f38ba8"  # 9.06:1,  #f38ba8
+    set -g green   "a6e3a1"  # 14.12:1, #a6e3a1
+    set -g yellow  "f9e2af"  # 16.52:1, #f9e2af
+    set -g orange  "fab387"  # 11.86:1, #fab387
+    set -g purple  "cba6f7"  # 10.33:1, #cba6f7
+    set -g cyan    "94e2d5"  # 14.09:1, #94e2d5
+    set -g pink    "f5c2e7"  # 13.75:1, #f5c2e7
+    set -g comment "9399b2"  # 7.43:1   #9399b2
+end

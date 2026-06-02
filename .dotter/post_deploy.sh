29f042d65ab7be54f75c272a86ffcadb11d37dda
Azzam S.A <noreply@azzamsa.com>
2026-06-02 13:30:23 +0700

    fix(wm/dms): run `mutagen` manually

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-CONFIG_FILE="$HOME/.config/emacs/config.el"
 FONT="{{font}}"
 
-sed -i "s|(setq +font-family \"[^\"]*\"|(setq +font-family \"$FONT\"|" "$CONFIG_FILE"
-echo "Done. set Emacs font to \"$FONT\""
+sed -i "s|(setq +font-family \"[^\"]*\"|(setq +font-family \"$FONT\"|" "$HOME/.config/emacs/config.el"
+echo "Done. Set Emacs font to '$FONT'"
 
 update-desktop-database ~/.local/share/applications
+echo "Done. Updated desktop databasese"
+
+matugen json wm/dms/themes/{{wm_theme}}.json
+echo "Done. Generated Matugen colors for '{{wm_theme}}'"

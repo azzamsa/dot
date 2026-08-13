86d1479a4ae7d2f6a38fa5123592f04cf25f63bb
Azzam S.A <noreply@azzamsa.com>
2026-08-14 05:19:00 +0700

    fix: let me use different font for each app

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-FONT="{{font}}"
-
-sed -i "s|(setq +font-family \"[^\"]*\"|(setq +font-family \"$FONT\"|" "$HOME/.config/emacs/config.el"
-echo "Done. Set Emacs font to '$FONT'"
-
-update-desktop-database ~/.local/share/applications
-echo "Done. Updated desktop databasese"
-
 matugen json wm/dms/themes/{{wm_theme}}.json
 echo "Done. Generated Matugen colors for '{{wm_theme}}'"
